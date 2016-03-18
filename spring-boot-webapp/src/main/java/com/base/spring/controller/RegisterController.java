79a7b099fce0ece5837d22f32c74b2c52db1919c
h819 <h81900@gmail.com>
2016-03-18 12:59:25 +0800

    misc update ...

---
+++ spring-boot-webapp/src/main/java/com/base/spring/controller/RegisterController.java
@@ -0,0 +1,24 @@
+package com.base.spring.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Description : TODO()
+ * User: h819
+ * Date: 2015/12/18
+ * Time: 12:49
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class RegisterController {
+
+    private static Logger logger = LoggerFactory.getLogger(RegisterController.class);
+     //http://www.baeldung.com/registration-verify-user-by-email?utm_source=email-newsletter&utm_medium=email&utm_campaign=auto_55_spring
+    //http://www.baeldung.com/spring-security-registration-verification-email?utm_source=email-newsletter&utm_medium=email&utm_campaign=auto_56_spring
+    /**
+     * 默认首页，直接跳转到相关的 Controller
+     * @return
+    */
+}
