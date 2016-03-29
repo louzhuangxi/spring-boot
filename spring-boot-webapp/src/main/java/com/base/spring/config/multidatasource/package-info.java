3104f52ebebb0ecec0bb27eb97ff55f98f15bccb
h819 <h81900@gmail.com>
2016-03-29 18:55:43 +0800

    misc

--- spring-boot-webapp/src/main/java/com/base/spring/config/multidatasource/package-info.java
+++ spring-boot-webapp/src/main/java/com/base/spring/config/multidatasource/package-info.java
@@ -2,6 +2,6 @@
  * 多数据源配置 :
  * 配置 MultiConfig1 , MultiConfig2 即可
  * Repository 和 Service 中不做任何配置，和单数据源一样。
- * Time: 16:47
+ * Time: 2016.03.29 :16:47
  */
 package com.base.spring.config.multidatasource;
---
+++ spring-boot-webapp/src/main/java/com/base/spring/config/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Description : TODO(配置文件)
+ * User: h819
+ * Date: 2016/3/29
+ * Time: 14:55
+ * To change this template use File | Settings | File Templates.
+ */
+package com.base.spring.config;
