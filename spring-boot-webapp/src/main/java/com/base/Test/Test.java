5418b8193c9adb98caa96fa2d635fbeb61a27d3f
h819 <h81900@gmail.com>
2016-12-16 16:33:28 +0800

    misc

--- spring-boot-webapp/src/main/java/com/base/Test/Test.java
+++ spring-boot-webapp/src/main/java/com/base/Test/Test.java
@@ -1,16 +1,13 @@
 package com.base.Test;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.time.DateUtils;
+import org.h819.commons.MyJsonUtils;
 import org.h819.commons.file.MyExcelUtils;
 import org.h819.commons.file.excel.poi.vo.ExcelLine;
 
 import java.io.File;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * Description : TODO()
@@ -21,21 +18,31 @@ import java.util.List;
  */
 public class Test {
 
+    private static List list = new ArrayList();
 
     public static void main(String[] args) throws Exception {
 
         Test test = new Test();
 
-        String[] ss ={"on","1","2"};
-        String[] ss1 ={"1","2"};
-        System.out.print(Arrays.asList(ArrayUtils.removeElement(ss1,"on")));
+        String[] ss = {"on", "1", "2"};
+        String[] ss1 = {"1", "2"};
+//        System.out.print(Arrays.asList(ArrayUtils.removeElement(ss1, "on")));
+//        System.out.print("中文");
 
-        System.out.print("中文");
+        test.listDir(new File("F:\\tianyiyun\\13811998709\\工作资料"));
 
-       // MyExcelUtils.writeExcel(test.findStandardEntityByCompany(new File("D:\\ftpFiles\\1.xlsx")), "sheet1", new File("D:\\ftpFiles\\5_out.xlsx"));
+        Collections.sort(list, new Comparator<MyFile>() {
+            @Override
+            public int compare(MyFile o, MyFile t1) {
+                return o.getSize().compareTo(t1.getSize());
+            }
+        });
 
-     //   System.out.print("中文");
-       // Assert.state("assc".equalsIgnoreCase("asc")|| "DESCs".equalsIgnoreCase("desc") , " 排序 direction 只能为 asc or desc");
+        MyJsonUtils.prettyPrint(list);
+        // MyExcelUtils.writeExcel(test.findStandardEntityByCompany(new File("D:\\ftpFiles\\1.xlsx")), "sheet1", new File("D:\\ftpFiles\\5_out.xlsx"));
+
+        //   System.out.print("中文");
+        // Assert.state("assc".equalsIgnoreCase("asc")|| "DESCs".equalsIgnoreCase("desc") , " 排序 direction 只能为 asc or desc");
     }
 
 
@@ -45,12 +52,13 @@ public class Test {
 
     /**
      * 演示如果在
+     *
      * @param fileInput
      * @return
      */
     public List<ExcelLine> findStandardEntityByCompany(File fileInput) {
 
-        List<ExcelLine> list = MyExcelUtils.readExcel(fileInput,new Integer(1), true);
+        List<ExcelLine> list = MyExcelUtils.readExcel(fileInput, new Integer(1), true);
         // 逐一循环企业 ，保存查询结果到 result
         List<ExcelLine> result = new ArrayList<>();
 
@@ -85,7 +93,7 @@ public class Test {
 
             if (companyname != null && !companyname.equals("")) {
                 result.add(s);
-               // MyJsonUtils.prettyPrint(s);
+                // MyJsonUtils.prettyPrint(s);
                 continue;
             }
 
@@ -109,4 +117,46 @@ public class Test {
         }
         return result;
     }
+
+    private void listDir(File dir) {
+
+        for (File file : dir.listFiles()) {
+
+            if (file.isFile()) {
+                list.add(new MyFile(file.getAbsolutePath(), file.length() / (1024*1024)));
+
+            } else listDir(file);
+
+        }
+
+
+    }
+
+    class MyFile {
+
+        String name;
+        Long size;
+
+        MyFile(String name, Long size) {
+            this.name = name;
+            this.size = size;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+    }
+
 }
